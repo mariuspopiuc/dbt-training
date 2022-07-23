dc7eb726f064e9a32d3a44456f88730b6ff1f278
mariuspopiuc@gmail.com <mariuspopiuc@gmail.com>
2022-07-23 17:20:48 +0000

    added delivery_team seed and added it to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -18,9 +18,12 @@ p.category,
 p.productname,
 p.subcategory,
 p.productid,
-{{ markup('ordersellingprice', 'ordercostprice') }} as markup
+{{ markup('ordersellingprice', 'ordercostprice') }} as markup,
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+left join {{ ref('delivery_team') }} as d
+on o.shipmode = d.shipmode
